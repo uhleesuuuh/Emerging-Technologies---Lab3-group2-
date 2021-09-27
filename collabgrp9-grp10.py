9b3f6983ceb0d9ced03e010e06383b85c9e0f3a7
adame_espiritu <group10@gmail.com>
2021-09-27 15:58:09 +0000

    This the commit of Second Group

--- collabgrp9-grp10.py
+++ collabgrp9-grp10.py
@@ -1,5 +1,6 @@
 # Laboratory Activity No. 3 Assignment
-# Author: Duran, Klibill & Delos Reyes, Nikki
+# Author: Duran, Klibill & Delos Reyes, Nikki (Group 1)
+# Author: Adame, Anamarie C. & Espiritu, Allysa Mae B. (Group 2)
 
 print("----------------------------------------------")
 print("           University Enrollment Form")
@@ -26,3 +27,31 @@ print("Other Fees\t\t\t:\t{}" .format(ofee))
 print("Tuition Fee\t\t\t:\t{}" .format(tfee))
 print("Gross Amount\t\t\t:\t{}" .format(gamount))
 print("----------------------------------------------")
+
+isko = str(input("Do you have a Scholarship?(Y/N)\t:\t"))
+
+if isko == "Y" or isko == "y":
+    disc = float(input ("Discount (%)\t\t\t:\t"))
+    tda = gamount * (disc/100)
+    print ("Total Discount Amount\t\t:\t{}" .format(tda))
+    
+    downp = int(input("Downpayment\t\t\t:\t"))
+
+    tbal = gamount - downp - tda
+    print("==============================================")
+    print("Total Balance\t\t\t:\t{}" .format(tbal))
+    print("==============================================")
+elif isko == "N" or isko == "n":
+    downp = int(input("Downpayment\t\t\t:\t"))
+
+    tbal = gamount - downp
+    print("==============================================")
+    print("Total Balance\t\t\t:\t{}" .format(tbal))
+    print("==============================================")
+else:
+    print("")
+    print("----------------Invalid Input!----------------")
+    
+    
+    
+    
